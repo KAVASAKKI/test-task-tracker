85bfcda47fba147e17de1d57b560639cc65fba19
KAVASAKKI <81836951+KAVASAKKI@users.noreply.github.com>
2022-07-19 01:43:50 +0300

    Added elements folder for reexport mui elements

---
+++ src/elements/index.ts
@@ -0,0 +1,5 @@
+export { PlayCircleOutlineIcon } from './mui-icons';
+export { PauseCircleOutlineIcon } from './mui-icons';
+export { RemoveCircleOutlineIcon } from './mui-icons';
+export { PlayCircleIcon } from './mui-icons';
+export { IconButton } from './mui-materials';
---
+++ src/elements/mui-icons/index.ts
@@ -0,0 +1,4 @@
+export { default as PlayCircleOutlineIcon } from '@mui/icons-material/PlayCircleOutline';
+export { default as PauseCircleOutlineIcon } from '@mui/icons-material/PauseCircleOutline';
+export { default as RemoveCircleOutlineIcon } from '@mui/icons-material/RemoveCircleOutline';
+export { default as PlayCircleIcon } from '@mui/icons-material/PlayCircle';
